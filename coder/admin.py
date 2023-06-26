e6210532e4099d246cdb24c62241b3f08df155c4
samuel Edelstein <sam@samuels-MacBook-Pro-2.local>
2023-06-26 15:20:23 -0500

    Update CoderAdmin and change_form templates, and add gray background to write_file block

--- coder/admin.py
+++ coder/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Coder
+from .models import Coder, CoderMessage
 from .interface import Interface
 
 
@@ -24,6 +24,6 @@ class CoderAdmin(admin.ModelAdmin):
     def render_change_form(self, request, context, *args, **kwargs):
         coder = kwargs.get('obj')
         context['adminform'].form.instance = coder
-        messages = Interface(coder.id).messages
-        context['coder_messages'] = list(map(lambda x: x.message_content, messages[1:]))
+        coder_messages = CoderMessage.objects.filter(coder=coder)
+        context['coder_messages'] = list(coder_messages.values())
         return super(CoderAdmin, self).render_change_form(request, context, *args, **kwargs)
